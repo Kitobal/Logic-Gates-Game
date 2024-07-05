d7170da31f04891b7fb2c8ef92d70d5d4e3a9389
Cristobal Fuentealba <114311709+Kitobal@users.noreply.github.com>
2024-07-05 16:48:59 -0400

    Added Moving Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,20 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] float moveSpeed = 5f;
+    Animator playerAnimator;
     Vector2 moveInput;
     Rigidbody2D playerRigidbody;
+    bool isMoving;
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Walk();
+        Animate();
     }
 
     void OnMove(InputValue value){
@@ -29,5 +33,18 @@ public class Player : MonoBehaviour
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
         playerRigidbody.velocity = playerVelocity;
 
+    }
+
+    void Animate(){
+        if (Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon|| Mathf.Abs(playerRigidbody.velocity.y) > Mathf.Epsilon){
+            isMoving = true;
+        } else{
+            isMoving = false;
+        }
+        playerAnimator.SetBool("IsMoving",isMoving);
+        if (isMoving){
+            playerAnimator.SetFloat("X",moveInput.x);
+            playerAnimator.SetFloat("Y",moveInput.y);
+        }
     }
 }
